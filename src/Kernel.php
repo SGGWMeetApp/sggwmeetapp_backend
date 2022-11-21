e534bde31574d8dae551602b2184749221559896
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2022-11-21 20:31:54 +0100

    Make UTC the default timezone for all DateTime objects

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,6 +5,8 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
+date_default_timezone_set('UTC');
+
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
