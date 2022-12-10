427887cdf915d294dc3134d882b38da02376a371
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2022-12-10 18:25:21 +0100

    Remove redundant methods from UserNotificationSettings class

--- src/Model/UserNotificationSettings.php
+++ src/Model/UserNotificationSettings.php
@@ -21,22 +21,4 @@ class UserNotificationSettings
         return $this->settings;
     }
 
-    public function getEnabledSettings(): array
-    {
-        $enabledSettings = [];
-        foreach($this->settings as $setting) {
-            if ($setting->isEnabled()) {
-                $enabledSettings [] = $setting;
-            }
-        }
-        return $enabledSettings;
-    }
-
-    public function getSettingByName(string $name): NotificationSetting {
-        foreach($this->settings as $setting) {
-            if ($setting->getName() === $name) return $setting;
-        }
-        throw new \OutOfBoundsException('Notification '.$name.' does not exist in user notification settings.');
-    }
-
 }
