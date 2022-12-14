53c67da6d6846e59d54b20ae8ab9430d119134e2
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2022-12-14 19:39:59 +0100

    Add `composer update` to php container startup script

--- .docker/php/startup.sh
+++ .docker/php/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 composer i -o
+composer update
 php bin/console lexik:jwt:generate-keypair --overwrite
 chmod 644 config/jwt/private.pem
 php bin/console messenger:consume async -vv >> /var/log/messenger.log 2>&1 &
