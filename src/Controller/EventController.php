9905e0f080b9fcf960f9b0d36b5ad1bc82ec2f8b
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2023-01-05 13:25:27 +0100

    Fix bug in joinEvent function of EventController

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -6,6 +6,7 @@ use App\Event\UserJoinedEventEvent;
 use App\Factory\NormalizerFactory;
 use App\Model\PrivateEvent;
 use App\Repository\EntityNotFoundException;
+use App\Repository\UserGroupRepositoryInterface;
 use App\Response\EventsResponse;
 use App\Service\SecurityHelper\JWTIdentityHelper;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -152,7 +153,12 @@ class EventController extends ApiController {
         return new EventsResponse('events', $this->normalizerFactory, $userAttendance, ...$events);
     }
 
-    public function joinEvent(int $event_id, int $user_id, JWTIdentityHelper $identityHelper): JsonResponse
+    public function joinEvent(
+        int $event_id,
+        int $user_id,
+        UserGroupRepositoryInterface $userGroupRepository,
+        JWTIdentityHelper $identityHelper
+    ): JsonResponse
     {
         $currentUser = $identityHelper->getUser();
         if($currentUser->getId() !== $user_id) {
@@ -164,7 +170,8 @@ class EventController extends ApiController {
             return $this->respondNotFound('Event not found.');
         }
         if ($event instanceof PrivateEvent) {
-            $userGroup = $event->getUserGroup();
+            $userGroupId = $event->getUserGroup()->getGroupId();
+            $userGroup = $userGroupRepository->findOrFail($userGroupId);
             if (!$userGroup->containsUser($currentUser)) {
                 return $this->respondUnauthorized();
             }
